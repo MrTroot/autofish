647b78089a747ade8ea518935a88335d97080be9
DrakoTrogdor <DrakoTrogdor@users.noreply.github.com>
2022-12-12 15:34:51 -0500

    Update for 1.19.3. Fix mixin and references due to PlaySoundIdS2CPacket removal. Bump version patch number.

--- src/main/java/troy/autofish/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/troy/autofish/mixin/MixinClientPlayNetworkHandler.java
@@ -4,7 +4,6 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
-import net.minecraft.network.packet.s2c.play.PlaySoundIdS2CPacket;
 import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -21,11 +20,6 @@ public class MixinClientPlayNetworkHandler {
     @Inject(method = "onPlaySound", at = @At("HEAD"))
     public void onPlaySound(PlaySoundS2CPacket playSoundS2CPacket_1, CallbackInfo ci) {
         if (client.isOnThread()) FabricModAutofish.getInstance().handlePacket(playSoundS2CPacket_1);
-    }
-
-    @Inject(method = "onPlaySoundId", at = @At("HEAD"))
-    public void onPlaySoundId(PlaySoundIdS2CPacket playSoundIdS2CPacket_1, CallbackInfo ci) {
-        if (client.isOnThread()) FabricModAutofish.getInstance().handlePacket(playSoundIdS2CPacket_1);
     }
 
     @Inject(method = "onEntityVelocityUpdate", at = @At("HEAD"))
