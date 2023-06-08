e27a91493801d6ec12647d75f99b7b5d052d0d96
Wudji <93850120+Wudji@users.noreply.github.com>
2023-06-08 21:09:27 +0800

    port to 1.20

--- src/main/java/troy/autofish/monitor/FishMonitorMPMotion.java
+++ src/main/java/troy/autofish/monitor/FishMonitorMPMotion.java
@@ -1,16 +1,16 @@
 package troy.autofish.monitor;
 
-import net.minecraft.block.Material;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
-import net.minecraft.util.function.MaterialPredicate;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import troy.autofish.Autofish;
+
 
 public class FishMonitorMPMotion implements FishMonitorMP {
 
@@ -30,7 +30,7 @@ public class FishMonitorMPMotion implements FishMonitorMP {
 
     @Override
     public void hookTick(Autofish autofish, MinecraftClient minecraft, FishingBobberEntity hook) {
-        if (worldContainsBlockWithMaterial(hook.world, hook.getBoundingBox(), Material.WATER)) {
+        if (worldContainsBlockWithMaterial(hook.getWorld(), hook.getBoundingBox())) {
             hasHitWater = true;
         }
     }
@@ -71,16 +71,18 @@ public class FishMonitorMPMotion implements FishMonitorMP {
         }
     }
 
-    public static boolean worldContainsBlockWithMaterial(World world, Box box, Material material) {
+    public static boolean worldContainsBlockWithMaterial(World world, Box box/*, Material material*/) {
         int i = MathHelper.floor(box.minX);
         int j = MathHelper.ceil(box.maxX);
         int k = MathHelper.floor(box.minY);
         int l = MathHelper.ceil(box.maxY);
         int m = MathHelper.floor(box.minZ);
         int n = MathHelper.ceil(box.maxZ);
-        MaterialPredicate materialPredicate = MaterialPredicate.create(material);
+        // MaterialPredicate materialPredicate = MaterialPredicate.create(material);
         return BlockPos.stream(i, k, m, j - 1, l - 1, n - 1).anyMatch((blockPos) -> {
-            return materialPredicate.test(world.getBlockState(blockPos));
+            // world.getBlockState(blockPos).contains(new Property<T>() {});
+            return world.getBlockState(blockPos).contains(Properties.LEVEL_8);
+            // return materialPredicate.test(world.getBlockState(blockPos));
         });
     }
 }
